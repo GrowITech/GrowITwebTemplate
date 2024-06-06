93e9a35cbfd5a40bb2510a91567fccb95f35d6b7
adrian-gfw <adrianabayon.gfw@gmail.com>
2024-06-06 17:43:55 +0800

    Created Homepage

---
+++ Activity 8 - Hospital Heatlhcare Template/assets/js/homeepage.js
@@ -0,0 +1,9 @@
+$(window).on("hashchange", function () {
+  $("div.tab").hide();
+  $(location.hash).fadeIn("slow");
+});
+
+$("a.hash").on("click", function (e) {
+  e.preventDefault();
+  location.hash = $(this).data("hash");
+});
