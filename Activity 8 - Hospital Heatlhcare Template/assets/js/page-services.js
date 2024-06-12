00421ebf6c2c10694395f1721512ba9fd8fb1b27
adrian-gfw <adrianabayon.gfw@gmail.com>
2024-06-12 16:09:30 +0800

    Created Services page and Responsive

---
+++ Activity 8 - Hospital Heatlhcare Template/assets/js/page-services.js
@@ -0,0 +1,33 @@
+//Hamburger Nav
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.getElementById("body");
+//Add new Hamburger on click
+const hamburgers = document.querySelector(".hamburgers");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.classList.toggle("fixed_body");
+
+  // document.querySelector("html").style.overflow = "hidden";
+});
+
+hamburgers.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.classList.toggle("fixed_body");
+
+  // document.querySelector("html").style.overflow = "hidden";
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    body.classList.remove("fixed_body");
+  })
+);
+
+//End: Hamburger Nav
