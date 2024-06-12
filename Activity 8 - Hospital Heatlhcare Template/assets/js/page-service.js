1e6a5a4d0cfd1be6fb9e7a38d1e88e5f176384af
adrian-gfw <adrianabayon.gfw@gmail.com>
2024-06-12 17:39:37 +0800

    Created the single page for services

---
+++ Activity 8 - Hospital Heatlhcare Template/assets/js/page-service.js
@@ -0,0 +1,63 @@
+//Hamburger Nav
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.getElementById("body");
+//Add new Hamburger on click
+const hamburgers = document.querySelector(".hamburgers");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.classList.toggle("fixed_body");
+
+  // document.querySelector("html").style.overflow = "hidden";
+});
+
+hamburgers.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.classList.toggle("fixed_body");
+
+  // document.querySelector("html").style.overflow = "hidden";
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    body.classList.remove("fixed_body");
+  })
+);
+
+//End: Hamburger Nav
+
+$(document).ready(function () {
+  $(".ul-doctors").slick({
+    infinite: true,
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
