96b714eb157a70fe1810ce1a7558e4161c668c9a
adrian-gfw <adrianabayon.gfw@gmail.com>
2024-06-13 11:01:33 +0800

    Created Doctors page and make it responsive

---
+++ Activity 8 - Hospital Heatlhcare Template/assets/js/page-doctors.js
@@ -0,0 +1,90 @@
+//Hamburger Nav
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.getElementById("body");
+//Add new Hamburger on click
+const hamburgers = document.querySelector(".hamburgers");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.classList.toggle("fixed_body");
+
+  // document.querySelector("html").style.overflow = "hidden";
+});
+
+hamburgers.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.classList.toggle("fixed_body");
+
+  // document.querySelector("html").style.overflow = "hidden";
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    body.classList.remove("fixed_body");
+  })
+);
+
+//End: Hamburger Nav
+
+$(document).ready(function () {
+  $(".ul-testimonial").slick({
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $(".ul-news").slick({
+    infinite: true,
+    slidesToShow: 2,
+    dots: true,
+    centerPadding: "40px",
+    rows: 2,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          centerPadding: "40px",
+          slidesToShow: 1,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerPadding: "40px",
+          slidesToShow: 1,
+          rows: 1,
+          variableWidth: true,
+        },
+      },
+    ],
+  });
+});
