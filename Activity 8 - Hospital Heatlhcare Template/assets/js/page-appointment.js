343b7161d70211629add35110a4bf50953bd61c8
adrian-gfw <adrianabayon.gfw@gmail.com>
2024-06-20 16:42:53 +0800

    Datepicker funcionalities done

--- Activity 8 - Hospital Heatlhcare Template/assets/js/page-appointment.js
+++ Activity 8 - Hospital Heatlhcare Template/assets/js/page-appointment.js
@@ -73,3 +73,51 @@ $(".li-contact-4").hover(
     $(this).find("img").attr("src", "assets/images/img-clock.png");
   }
 );
+
+// DatePicker Restriction Dates
+
+$(function () {
+  var dtToday = new Date();
+  var $dateInput = $("#txtDate");
+
+  var month = dtToday.getMonth() + 1;
+  var day = dtToday.getDate();
+  var year = dtToday.getFullYear();
+  var mm = String(dtToday.getMonth() + 1).padStart(2, "0"); // January is 0
+  var dd = String(dtToday.getDate()).padStart(2, "0");
+  if (month < 10) month = "0" + month.toString();
+  if (day < 10) day = "0" + day.toString();
+  var maxDate = year + "-" + month + "-" + day;
+  var minDate = year + "-" + mm + "-" + dd; // Minimum date is today
+
+  // or instead:
+  // var maxDate = dtToday.toISOString().substr(0, 10);
+
+  // alert(maxDate);
+  $("#txtDate").attr("min", maxDate);
+
+  //
+  //
+  //
+  //
+  // Codes in Disabling Sundays
+  // Function to check if a given date is a Sunday
+  function isSunday(date) {
+    var dayOfWeek = new Date(date).getDay();
+    return dayOfWeek === 0; // Sunday is represented by 0
+  }
+
+  // Disable Sundays in the date picker
+  function disableSundays() {
+    $('input[type="date"]').on("change", function () {
+      var selectedDate = $(this).val();
+      if (isSunday(selectedDate)) {
+        alert("We are closed on Sundays. Please select another date.");
+        $(this).val("");
+      }
+    });
+  }
+
+  // Call disableSundays function on document ready
+  disableSundays();
+});
